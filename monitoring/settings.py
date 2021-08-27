1eb242206886f2517212cc3d2ed739e653542754
Felipe Bedoya <f.bedoya@uniandes.edu.co>
2021-08-26 20:43:53 -0500

    Update settings.py

--- monitoring/settings.py
+++ monitoring/settings.py
@@ -78,11 +78,11 @@ WSGI_APPLICATION = 'monitoring.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "<nombrebd>",
-        "USER": "<usuario>",
-        "PASSWORD": "<clave>",
+        "NAME": "bdtaller1",
+        "USER": "fbedoya",
+        "PASSWORD": "Prometeo1155",
         "HOST": "localhost",
-        "PORT": "",
+        "PORT": "5432",
     }
 }
 
